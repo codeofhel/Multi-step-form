2dacefabd1e7303586018a3f88c4388cda364373
spektrodude <ifhelio_carvalho@hotmail.com>
2023-06-26 10:50:54 +0100

    adição de pagina de sucesso

--- script.js
+++ script.js
@@ -16,56 +16,7 @@ const yearly = {
     "customizableProfile": { "value": "+$20/yr" },
 }
 
-/*----- Change values of Plan------- */
 
-document.querySelector("#timemodel").addEventListener("change", (el) => {
-    
-    if (el.target.checked) {
-        document.querySelectorAll("[data-value]").forEach((elem) => {
-            const yearlyValues = yearly[elem.dataset.value]
-            elem.textContent = yearlyValues["value"];
-            yearlyValues["discount"] != null ? elem.insertAdjacentHTML("afterend", `<p class="discount fs-100 ff__regular">${yearlyValues["discount"]}</p>`) : "";
-        })
-    } else {
-        document.querySelectorAll("[data-value]").forEach((elem) => {
-            const monthlyValues = monthly[elem.dataset.value]
-            elem.textContent = monthlyValues["value"];
-            document.querySelectorAll(".discount")?.forEach((el) => el.remove())
-        })
-    }
-    updateData();
-    displayFinishBoard();
-})
-
-/*-----/Change values of Plan------- */
-
-/*-----Change style of cards on click-------*/
-document.querySelectorAll(".select__plan .card").forEach((elem) => {
-    elem.addEventListener("click", function () {
-        document.querySelectorAll(".select__plan .card.checked").forEach((el) => el.classList.remove("checked"));
-        elem.classList.add("checked");
-        addPlan(elem)
-        displayFinishBoard()
-    });
-})
-
-        /*change style in pick add ons step*/
-document.querySelectorAll(".pick__addons .card").forEach((elem) => {
-    elem.addEventListener("click", () => {
-        elem.classList.toggle("checked");
-        addRemoveAddOns();
-        validateInputCheck(elem)
-        displayFinishBoard()
-    });
-})
-
-function validateInputCheck(elem) {
-    /*checkbox checked when click on parent div*/
-    const checboxInput = elem.querySelector(".addOn");
-    elem.classList.contains("checked") ? checboxInput.checked = true : checboxInput.checked = false;
-}
-
-/*-----/Change style of cards on click-------*/
 
 
 /*----Finish Board---*/
@@ -108,30 +59,36 @@ function displayFinishBoard() {
     const summaryPlan = document.querySelector(".summaryPlan");
     const summaryPlanValue = document.querySelector(".summaryPlanValue");
     const addonsList = document.querySelector(".add_ons");
-    
+
     summaryPlan.textContent = finishBoardInfo.plan.description;
     summaryPlanValue.textContent = finishBoardInfo.plan.value;
     addonsList.innerHTML = "";
     finishBoardInfo.addOns.map((elem) => {
-        
+
         const summaryAddon = `<div><p class='summaryAddon clr-secondary-text fs-200 ff__regular'>${elem.description}</p><p class='summaryAddonValue fs-200 ff__regular'>${elem.value}</p></div>`;
         addonsList.insertAdjacentHTML("beforeend", summaryAddon);
     })
-    
+
     function calculateTotal() {
         const planValue = parseFloat(finishBoardInfo.plan.value.slice(1));
-        var boi = finishBoardInfo.addOns.reduce((acc,elem) => {
+        var addOnsTotal = finishBoardInfo.addOns.reduce((acc, elem) => {
             return acc + parseFloat(elem.value.slice(2));
-        },0)
-        const total = boi + planValue;
-        document.querySelector(".totalpay").textContent = `$${total}/yr` 
+        }, 0)
+        const total = addOnsTotal + planValue;
+        document.querySelector(".totalpay").textContent = `$${total}/yr`
     }
     calculateTotal();
 }
 
 /*----/Finish Board---*/
 
-function changeStep(n) {
+function handleErrors(el) {
+    el.classList?.remove("inputs", "clr__error");
+    el.previousElementSibling.querySelector("span").style.display = "none";
+}
+
+function changeStep(n, e) {
+
     const currentCard = document.querySelector("[data-currentCard]");
     const currentCardVal = currentCard.dataset["currentcard"];
     const currentCardElement = document.querySelector("[data-id=" + currentCardVal + "]");
@@ -139,9 +96,12 @@ function changeStep(n) {
     const selectedCard_id = selectedCardElement.dataset.id;
     const btns = document.querySelector(".previous__next");
 
-    btns.querySelectorAll(".nav__btn").forEach((elem) => elem.classList.add("hide"))
 
-    currentCardElement.classList.add("hide"); /*hide the old card*/
+    function hideStuff() {
+        btns.querySelectorAll(".nav__btn,thankyouWindow").forEach((elem) => elem.classList.add("hide"))
+        currentCardElement.classList.add("hide"); /*hide the old card*/
+    }
+
 
     function selectedCardEl() {
         if (n == "nextCard")
@@ -165,26 +125,135 @@ function changeStep(n) {
         document.querySelector(".menu__bullet[data-step=" + idCard + "]").classList.add("bulletOn")
     }
 
+    function verifyInputs(e) {
+        const emptyInputs = Array.from(document.querySelectorAll(".personal__info input")).filter((inputEl) => {
+            if (inputEl.value == "") {
+                inputEl.previousElementSibling.querySelector("span").style.display = "inline-block";
+                inputEl.classList.add("inputs", "clr__error");
+                return inputEl;
+            }
+        })
+
+        if (emptyInputs.length)
+            return false
+        return true;
+    }
+
+    function verifyPlan() {
+        const emptyPlan = document.querySelectorAll(".select__plan .card.checked");
+
+        if (!emptyPlan.length)
+            return false
+        return true;
+    }
+    
+    function verifyAddnonsChecked() {
+        const emptyAdds = document.querySelectorAll(".pick__addons .card.checked");
+
+        if (!emptyAdds.length)
+            return false
+        return true;
+    }
+
+    function confirmSubscription() {
+        document.querySelector(".confirm__btn").addEventListener("click", function () {
+            document.querySelector(".finish__board").classList.add("hide");
+            document.querySelector(".thankyouWindow").classList.remove("hide");
+            btns.querySelectorAll(".nav__btn").forEach((btn)=>btn.classList.add("hide"))
+        })
+    }
+    
     const obj = {
         "personal_info": function () {
+            hideStuff();
             handleButtons(".next");
             handleSteps();
         },
         "select_plan": function () {
+            if (!verifyInputs())
+                return false;
+            hideStuff();
             handleButtons(".prev,.next");
             handleSteps();
         },
         "pick_addons": function () {
+            if (!verifyInputs() || !verifyPlan())
+                return false;
+            hideStuff();
             handleButtons(".prev,.next");
             handleSteps();
         },
         "finish_board": function () {
+            if (!verifyInputs() || !verifyPlan() || !verifyAddnonsChecked())
+                return false;
+            hideStuff();
             handleButtons(".prev,.confirm__btn");
             handleSteps();
+            confirmSubscription();
         }
     }
 
     obj[selectedCard_id]();
 }
 
+/*----- Change values of Plan------- */
+
+document.querySelector("#timemodel").addEventListener("change", (el) => {
+
+    if (el.target.checked) {
+        document.querySelectorAll("[data-value]").forEach((elem) => {
+            const yearlyValues = yearly[elem.dataset.value]
+            elem.textContent = yearlyValues["value"];
+            yearlyValues["discount"] != null ? elem.insertAdjacentHTML("afterend", `<p class="discount fs-100 ff__regular">${yearlyValues["discount"]}</p>`) : "";
+        })
+    } else {
+        document.querySelectorAll("[data-value]").forEach((elem) => {
+            const monthlyValues = monthly[elem.dataset.value]
+            elem.textContent = monthlyValues["value"];
+            document.querySelectorAll(".discount")?.forEach((el) => el.remove())
+        })
+    }
+    updateData();
+    displayFinishBoard();
+})
+
+/*-----/Change values of Plan------- */
+
+/*-----Change style of cards on click-------*/
+document.querySelectorAll(".select__plan .card").forEach((elem) => {
+    elem.addEventListener("click", function () {
+        document.querySelectorAll(".select__plan .card.checked").forEach((el) => el.classList.remove("checked"));
+        elem.classList.add("checked");
+        addPlan(elem)
+        displayFinishBoard()
+    });
+})
+
+/*change style in pick add ons step*/
+document.querySelectorAll(".pick__addons .card").forEach((elem) => {
+    elem.addEventListener("click", () => {
+        elem.classList.toggle("checked");
+        addRemoveAddOns();
+        elem.classList.contains("checked") ? elem.querySelector(".addOn").checked = true : elem.querySelector(".addOn").checked = false; /*checkbox checked when click on parent div*/
+        displayFinishBoard()
+    });
+})
+
+/*-----/Change style of cards on click-------*/
+
+
+document.querySelector('.nav__btn.prev').addEventListener("click", (e) => {
+    changeStep("prevCard", e)
+});
+
+document.querySelector('.next').addEventListener("click", (e) => {
+    changeStep("nextCard");
+});
+
+document.querySelectorAll(".menu__bullet").forEach((elem) => {
+    elem.addEventListener("click", () => changeStep(elem.dataset.step))
+})
+
+document.querySelectorAll(".personal__info input").forEach((elem) => elem.addEventListener("focus", () => handleErrors(elem)))
 changeStep("personal_info");
+
